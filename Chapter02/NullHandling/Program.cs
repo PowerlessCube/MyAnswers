74e68a220077dc8bcdb250f0568474742676545d
Alistair Mackay <alistair.mackay.work@gmail.com>
2022-03-10 22:47:26 +0000

    stopped at page 96

--- Chapter02/NullHandling/Program.cs
+++ Chapter02/NullHandling/Program.cs
@@ -1,4 +1,6 @@
-﻿int thisCannotBeNull = 4;
+﻿#nullable enable
+
+int thisCannotBeNull = 4;
 // thisCannotBeNull = null; //compile error!
 
 int? thisCouldBeNull = null;
@@ -8,3 +10,21 @@ Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
 thisCouldBeNull = 7;
 Console.WriteLine(thisCouldBeNull);
 Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+var address = new Address();
+address.Building = null;
+address.Street = null;
+address.City = "London";
+address.Region = null;
+
+var result = address.Street?.Length ?? 3;
+Console.WriteLine(result);
+
+
+class Address
+{
+  public string? Building;
+  public string Street = String.Empty;
+  public string City = String.Empty;
+  public string Region = String.Empty; 
+}

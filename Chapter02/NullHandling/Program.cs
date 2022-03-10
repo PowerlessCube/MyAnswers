7a5f2038e595cafed7b398e265bd4d15faa4d767
Alistair Mackay <alistair.mackay.work@gmail.com>
2022-03-10 22:18:39 +0000

    init commit of study of Mark J. Price's c# and .Net 5 Modern Cross-Platform Development.

---
+++ Chapter02/NullHandling/Program.cs
@@ -0,0 +1,10 @@
+﻿int thisCannotBeNull = 4;
+// thisCannotBeNull = null; //compile error!
+
+int? thisCouldBeNull = null;
+Console.WriteLine(thisCouldBeNull);
+Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+thisCouldBeNull = 7;
+Console.WriteLine(thisCouldBeNull);
+Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Xml;
+
+object height = 1.88;
+object name = "Amir";
+dynamic anotherName = "Ahmed";
+Console.WriteLine($"{name} is {height} metres tall.");
+
+// int length1 = name.Length;
+int length = anotherName.Length;
+int length2 = ((string)name).Length;
+Console.WriteLine($"{name} has {length2} characters.");
+
+// var population = 66_000_000;
+// var weight = 1.88;
+// var price = 4.95M;
+// var fruit = "Apples";
+// var letter = 'Z';
+// var happy = true;
+
+// var xml1 = new XmlDocument();
+// XmlDocument XMl2 = new XmlDocument();
+
+// var file1 = File.CreateText(@"C:\something.text");
+// StreamWriter file2 = File.CreateText(@"C:\something.txt");
+
+// XmlDocument xml3 = new();
+
+Console.WriteLine($"default(int) = {default(int)}");
+Console.WriteLine($"default(bool) = {default(bool)}");
+Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+Console.WriteLine($"default(string) = {default(string)}");
+
